de19689b8b92e99bcd65dc01a38c2864d5888d5e
katie leong <katieleong@yahoo.com>
2024-08-28 22:21:26 -0700

    set up redux store in store.js

---
+++ frontend/src/store/store.js
@@ -0,0 +1,19 @@
+// setup Redux Store
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { thunk } from 'redux-thunk';
+
+const rootReducer = combineReducers({});
+
+let enhancer;
+
+if (import.meta.env.MODE === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = (await import('redux-logger')).default;
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+export default function configureStore(preloadedState) {
+  return createStore(rootReducer, preloadedState, enhancer);
+}
