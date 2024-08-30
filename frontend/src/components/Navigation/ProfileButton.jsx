7c54c047c2f9b3f62d69f9253e3f7bbb5ea1cdc9
katie leong <katieleong@yahoo.com>
2024-08-29 23:14:38 -0700

    install react-icons

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,11 @@
+import { FaCarrot } from 'react-icons/fa6';
+
+export default function ProfileButton() {
+  const Carrot = () => {
+    return (
+      <div style={{ color: 'orange', fontSize: '100px' }}>
+        <FaCarrot />
+      </div>
+    );
+  };
+}
