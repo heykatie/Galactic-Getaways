b3c624856c95a741432b6f874a6ead3ba37558ca
katie leong <katieleong@yahoo.com>
2024-08-30 16:23:21 -0700

    redid up to phase3

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,27 +1,15 @@
 import './Navigation.css';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector((state) => state.session.user);
-	const dispatch = useDispatch();
-
-	const logout = (e) => {
-		e.preventDefault();
-		dispatch(sessionActions.logout());
-	};
 
 	const sessionLinks = sessionUser ? (
-		<>
-			<li>
-				<ProfileButton user={sessionUser} />
-			</li>
-			<li>
-				<button onClick={logout}>Log Out</button>
-			</li>
-		</>
+		<li>
+			<ProfileButton user={sessionUser} />
+		</li>
 	) : (
 		<>
 			<li>
---
+++ frontend/src/context/Modal.jsx
@@ -0,0 +1,14 @@
+import { useRef, createContext } from 'react';
+
+const ModalContext = createContext();
+
+export function ModalProvider({ children }) {
+	const modalRef = useRef();
+
+	return (
+		<>
+			<ModalContext.Provider>{children}</ModalContext.Provider>
+			<div ref={modalRef} />
+		</>
+	);
+}
