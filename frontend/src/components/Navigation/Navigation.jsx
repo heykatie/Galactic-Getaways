516b2ac0d7a400c643cab2e0d5a7cf2694f7641b
katie leong <katieleong@yahoo.com>
2024-08-30 17:33:38 -0700

    refactor navigation component to render the ProfileButton component even when no session user. remove OpenModalButton

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,49 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import OpenModalButton from '../OpenModalButton';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
+	const sessionUser = useSelector((state) => state.session.user);
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    );
-  } else {
-		sessionLinks = (
-			<>
+	return (
+		<ul>
+			<li>
+				<NavLink to='/'>Home</NavLink>
+			</li>
+			{isLoaded && (
 				<li>
-					<OpenModalButton
-						buttonText="Log In"
-						modalComponent={<LoginFormModal />}
-					/>
+					<ProfileButton user={sessionUser} />
 				</li>
-				<li>
-					<OpenModalButton
-						buttonText="Sign Up"
-						modalComponent={<SignupFormModal />}
-					/>
-				</li>
-			</>
-
-    );
-  }
-
-  return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      {isLoaded && sessionLinks}
-    </ul>
-  );
+			)}
+		</ul>
+	);
 }
 
 export default Navigation;
