459bdb4873608cc9c95c996a3fc6f7c8a9fb14c3
Katie Leong <155786253+heykatie@users.noreply.github.com>
2024-08-30 21:42:25 -0700

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,33 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
 // redux provider
 import { Provider } from 'react-redux';
-import configureStore from './store/index';
-
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import configureStore from './store';
+import { Modal, ModalProvider } from './context/Modal';
 
 // session actions & reducer
 import * as sessionActions from './store/session';
 
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 // setup store
 const store = configureStore();
-if (process.env.NODE_ENV !== 'production') window.store = store;
 
 // call the restoreCSRF function when in development & attach the custom csrfFetch function onto the window
 if (import.meta.env.MODE !== 'production') {
 	restoreCSRF();
 
 	window.csrfFetch = csrfFetch;
-  window.store = store;
-  window.sessionActions = sessionActions;
+	window.store = store;
+	window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ModalProvider>
+			<Provider store={store}>
+				<App />
+				<Modal />
+			</Provider>
+		</ModalProvider>
+	</React.StrictMode>
 );
