6e33c03420732584d486258b7e8388793eec3453
katie leong <katieleong@yahoo.com>
2024-08-30 17:26:55 -0700

    login form modal

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,9 @@
-import './Navigation.css';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import './Navigation.css';
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector((state) => state.session.user);
@@ -13,7 +15,10 @@ function Navigation({ isLoaded }) {
 	) : (
 		<>
 			<li>
-				<NavLink to='/login'>Log In</NavLink>
+				<OpenModalButton
+					buttonText='Log In'
+					modalComponent={<LoginFormModal />}
+				/>
 			</li>
 			<li>
 				<NavLink to='/signup'>Sign Up</NavLink>
