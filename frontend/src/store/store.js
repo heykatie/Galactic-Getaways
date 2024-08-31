fe5c751fb8e06fbff8a9a8e367a1e82796d12116
Katie Leong <155786253+heykatie@users.noreply.github.com>
2024-08-30 21:47:28 -0700

    update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,18 +3,23 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 import sessionReducer from './session';
 
-const rootReducer = combineReducers({ session: sessionReducer});
+const rootReducer = combineReducers({
+	session: sessionReducer,
+});
 
 let enhancer;
 
 if (import.meta.env.MODE === 'production') {
-  enhancer = applyMiddleware(thunk);
+	enhancer = applyMiddleware(thunk);
 } else {
-  const logger = (await import('redux-logger')).default;
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+	const logger = (await import('redux-logger')).default;
+	const composeEnhancers =
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-export default function configureStore(preloadedState) {
-  return createStore(rootReducer, preloadedState, enhancer);
-}
+const configureStore = (preloadedState) => {
+	return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
