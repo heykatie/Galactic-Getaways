ece0a5f29961a81805d8fb55a60564f510bb3a8c
katie leong <katieleong@yahoo.com>
2024-08-30 17:18:29 -0700

    add modal context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 // redux provider
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { Modal, ModalProvider } from './context/Modal';
 
 // session actions & reducer
 import * as sessionActions from './store/session';
@@ -19,14 +20,17 @@ if (import.meta.env.MODE !== 'production') {
 	restoreCSRF();
 
 	window.csrfFetch = csrfFetch;
-  window.store = store;
-  window.sessionActions = sessionActions;
+	window.store = store;
+	window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<ModalProvider>
+			<Provider store={store}>
+				<App />
+				<Modal />
+			</Provider>
+		</ModalProvider>
 	</React.StrictMode>
 );
