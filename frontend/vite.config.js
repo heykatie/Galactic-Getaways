d02228cda9b839ac25dab729c4222359e8073024
Katie Leong <155786253+heykatie@users.noreply.github.com>
2024-08-30 21:48:27 -0700

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,20 +3,19 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    eslint({
-      lintOnStart: true,
-      failOnError: mode === "production"
-    })
-  ],
-  // To automatically open the app in the browser whenever the server starts,
-  // uncomment the following lines:
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000'
-    },
-    open: true
-  }
+	plugins: [
+		react(),
+		eslint({
+			lintOnStart: true,
+			failOnError: mode === 'production',
+		}),
+	],
+	server: {
+		proxy: {
+			'/api': 'http://localhost:8000',
+		},
+		// open: true,
+	},
 }));
