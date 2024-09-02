c1d873ca3c8ea0f538059361ab61896cae821b0e
Katie Leong <155786253+heykatie@users.noreply.github.com>
2024-09-01 21:52:07 -0700

    create spots component

--- backend/db/seeders/20240806064554-spot-images.js
+++ backend/db/seeders/20240806064554-spot-images.js
@@ -4,36 +4,36 @@
 
 const { SpotImage } = require('../models');
 const spotImages = [
-  {
-    spotId: 1,
-    url: 'https://example1.com/image1.jpg',
-    preview: true,
-  },
+	{
+		spotId: 1,
+		url: 'https://global.discourse-cdn.com/mcneel/uploads/default/original/4X/a/8/7/a8724ce18fa4d750a247eb31c5df48b8aa2966ee.jpeg',
+		preview: true,
+	},
 
-  {
-    spotId: 2,
-    url: 'https://example2.com/image2.jpg',
-    preview: true,
-  },
+	{
+		spotId: 2,
+		url: 'https://hips.hearstapps.com/hmg-prod/images/edc100123egan-002-6500742f5feb7.jpg?crop=0.688xw:1.00xh;0.157xw,0&resize=980:*',
+		preview: true,
+	},
 
-  {
-    spotId: 3,
-    url: 'https://example3.com/image3.jpg',
-    preview: true,
-  },
+	{
+		spotId: 3,
+		url: 'https://www.the-sun.com/wp-content/uploads/sites/6/2022/07/NINTCHDBPICT000750103510.jpg?strip=all&w=960',
+		preview: true,
+	},
 
-  {
-    spotId: 4,
-    url: 'https://example4.com/image4.jpg',
-    preview: true,
-  },
+	{
+		spotId: 4,
+		url: 'https://global.discourse-cdn.com/mcneel/uploads/default/original/4X/9/d/8/9d8a709128f30a273ff16329c91667f12e9b960b.jpeg',
+		preview: true,
+	},
 
-  {
-    spotId: 5,
-    url: 'https://example5.com/image5.jpg',
-    preview: true,
-  },
-]
+	{
+		spotId: 5,
+		url: 'https://www.pcgamesn.com/wp-content/sites/pcgamesn/2024/02/minecraft-houses-550x309.jpg',
+		preview: true,
+	},
+];
 
 
 module.exports = {
