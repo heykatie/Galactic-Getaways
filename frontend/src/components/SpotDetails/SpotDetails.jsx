89dfbe89a0da8369b811db6f8a166f17d3756acc
Katie Leong <155786253+heykatie@users.noreply.github.com>
2024-09-03 00:08:38 -0700

    added main image to spots details page

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -1,18 +1,40 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-// import { selectInnById, getInnById } from '../../store/innsSlice';
-// import { useEffect } from 'react';
+import { getSpotById } from '../../store/spots';
+import { useEffect } from 'react';
 // import NotFound from '../404';
 // import Reviews from '../Reviews';
 // import { GiAlienFire } from 'react-icons/gi';
 
 export default function SpotDetails() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
   const spots = useSelector(state => state.spots)
+  const spot = spots[id];
+
+  const images = spot?.SpotImages?.filter((image) => image.preview === false);
+
   const reviews = useSelector(state => state.reviews)
 
-	return (
-		<div>
-			<h1>hi</h1>
-		</div>
+  useEffect(() => {
+    dispatch(getSpotById(id));
+  }, [dispatch, id]);
+
+  if (!spot) return <h1>No longer available.</h1>;
+  if (!images) return <h1>Loading...</h1>;
+
+  return (
+    <div>
+      <div id='spot-info'>
+        <h1>{spot.name}</h1>
+        <p>{spot.city}, {spot.state}, {spot.country}</p>
+      </div>
+      <div id='images'>
+        <div id='preview-img'>
+          <img id='preview' src={spot.previewImage} alt={spot.description}/>
+        </div>
+      </div>
+    </div>
 	);
 }
