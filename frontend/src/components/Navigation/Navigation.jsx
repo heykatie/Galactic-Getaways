65fa26419ff05263f1edf653e04235d8e40f7253
Katie Leong <155786253+heykatie@users.noreply.github.com>
2024-09-20 19:51:49 -0700

    profile dropdown menu overlays

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,35 +1,35 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import './Navigation.css';
 import logo from '../../../../images/logo.png';
 import name from '../../../../images/name.jpeg';
+import nav from './Navigation.module.css';
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector((state) => state.session.user);
 
 	return (
-		<div className='nav'>
-			<ul className='navbar'>
-				<li id='logo-name'>
+		<div className={nav.nav}>
+			<ul className={nav.navbar}>
+				<li id={nav.left}>
 					<NavLink to='/'>
-						<img src={logo} alt='logo' />
+						<img src={logo} alt='logo' id={nav.logo} />
 					</NavLink>
 					<NavLink to='/'>
-						<img src={name} alt='name' />
+						<img src={name} alt='name' id={nav.name} />
 					</NavLink>
 				</li>
 
 				{isLoaded && (
-					<div id='right-nav'>
+					<div id={nav.right}>
 						{sessionUser && (
 							<li>
-								<NavLink id='create-spot' to='/spots/new'>
+								<NavLink id={nav.createSpot} to='/spots/new'>
 									Create a New Spot
 								</NavLink>
 							</li>
 						)}
-						<li id='user-button'>
+						<li id={nav.userButton}>
 							<ProfileButton user={sessionUser} />
 						</li>
 					</div>
